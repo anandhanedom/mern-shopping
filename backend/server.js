3f61c6a8f01b4acba40a852362b3cf6f72854a67
Anandhan <anandhanedom@gmail.com>
2021-06-29 21:08:33 +0530

    asdadsdadsadsadsads

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,14 @@
-import express from "express";
+import colors from "colors";
+import connectDB from "./config/db.js";
 import dotenv from "dotenv";
+import express from "express";
 import products from "./data/products.js";
 
-const app = express();
-
 dotenv.config();
+
+connectDB();
+
+const app = express();
 
 app.get("/", (req, res) => res.send("API is running...."));
 
@@ -21,5 +25,5 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
 );
