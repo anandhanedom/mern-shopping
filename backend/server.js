a9b41e0ff72aed579bfc58cc36da78bde781b1de
Anandhan <anandhanedom@gmail.com>
2021-06-30 13:12:40 +0530

    feat: make products and single product get routes

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ import colors from "colors";
 import connectDB from "./config/db.js";
 import dotenv from "dotenv";
 import express from "express";
-import products from "./data/products.js";
+import productRoutes from "./routes/productRoutes.js";
 
 dotenv.config();
 
@@ -12,18 +12,13 @@ const app = express();
 
 app.get("/", (req, res) => res.send("API is running...."));
 
-app.get("/api/products", (req, res) => {
-  res.json(products);
-});
-
-app.get("/api/products/:id", (req, res) => {
-  const product = products.find((product) => product._id === req.params.id);
-  res.json(product);
-});
+app.use("/api/products", productRoutes);
 
 const PORT = process.env.PORT || 5000;
 
 app.listen(
   PORT,
-  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold)
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
 );
