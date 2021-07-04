c7006ae0a6335c644943eb615d6e28f0bb887e15
Anandhan <anandhanedom@gmail.com>
2021-07-04 19:25:48 +0530

    feat: implement redux for products list

---
+++ frontend/src/reducers/productReducers.js
@@ -0,0 +1,28 @@
+import {
+  PRODUCT_LIST_FAILURE,
+  PRODUCT_LIST_SUCCESS,
+  PRODUCT_LIST_REQUEST,
+} from "../constants/productConstants";
+
+export const productListReducer = (
+  state = { products: [], loading: false, error: "" },
+  action
+) => {
+  switch (action.type) {
+    case PRODUCT_LIST_REQUEST:
+      return { ...state, loading: true };
+
+    case PRODUCT_LIST_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        products: action.payload,
+      };
+
+    case PRODUCT_LIST_FAILURE:
+      return { ...state, loading: false, error: action.payload };
+
+    default:
+      return state;
+  }
+};
