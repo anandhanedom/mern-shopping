fb3d9c6761cbdc63069a440f7b369b654b353b8c
Anandhan <anandhanedom@gmail.com>
2021-07-10 16:29:34 +0530

    fix: add default values

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,6 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  Row,
+  ListGroup,
+  Col,
+  Image,
+  Form,
+  Button,
+  Card,
+} from "react-bootstrap";
+import Messages from "../components/Message";
+import { addToCart } from "../actions/cartActions";
 
-const CartScreen = () => {
+const CartScreen = ({ match, location }) => {
+  const productId = match.params.id;
+  const history = useHistory();
+
+  const qty = location.search;
+
   return <div>Cart</div>;
 };
 
