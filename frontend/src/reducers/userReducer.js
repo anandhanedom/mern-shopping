fc61df0e8d7a0ec389b3d9bb730aac53f9bcd999
Anandhan <anandhanedom@gmail.com>
2021-08-04 20:25:08 +0530

    feat: implement user register reducer,action and screen

--- frontend/src/reducers/userReducer.js
+++ frontend/src/reducers/userReducer.js
@@ -2,6 +2,9 @@ import {
   USER_LOGIN_FAILURE,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
+  USER_REGISTER_FAILURE,
+  USER_REGISTER_REQUEST,
+  USER_REGISTER_SUCCESS,
   USER_LOGOUT,
 } from "../constants/userConstants.js";
 
@@ -29,6 +32,29 @@ export const userLoginReducer = (
         loading: false,
         userInfo: null,
       };
+
+    default:
+      return state;
+  }
+};
+
+export const userRegisterReducer = (
+  state = { userInfo: null, loading: false, error: false },
+  action
+) => {
+  switch (action.type) {
+    case USER_REGISTER_REQUEST:
+      return { ...state, loading: true };
+
+    case USER_REGISTER_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        userInfo: action.payload,
+      };
+
+    case USER_REGISTER_FAILURE:
+      return { ...state, loading: false, error: action.payload };
 
     default:
       return state;
---
+++ frontend/src/screens/RegisterScreen.js
@@ -0,0 +1,96 @@
+import React, { useState, useEffect } from "react";
+import { Link, useHistory } from "react-router-dom";
+import { Form, Button, Row, Col, FormGroup } from "react-bootstrap";
+import { useDispatch, useSelector } from "react-redux";
+import Message from "../components/Message";
+import Loader from "../components/Loader";
+import { register } from "../actions/userActions";
+import FormContainer from "../components/FormContainer";
+
+const RegisterScreen = ({ location }) => {
+  const dispatch = useDispatch();
+  const history = useHistory();
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+
+  const userRegister = useSelector((state) => state.userRegister);
+  const { loading, error, userInfo } = userRegister;
+
+  const redirect = location.search ? location.search.split("=")[1] : "/";
+
+  useEffect(() => {
+    if (userInfo) {
+      history.push(redirect);
+    }
+  }, [history, userInfo, redirect]);
+
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    if (password !== confirmPassword) {
+      setMessage("Passwords do not match");
+    } else {
+      dispatch(register(name, email, password));
+    }
+  };
+
+  return (
+    <FormContainer>
+      <h1>Sign Up</h1>
+      {message ? <Message variant="danger">{message}</Message> : null}
+      {error ? <Message variant="danger">{error}</Message> : null}
+      {loading ? <Loader /> : null}
+      <Form onSubmit={submitHandler}>
+        <FormGroup controlId="name">
+          <Form.Label>Name</Form.Label>
+          <Form.Control
+            type="name"
+            placeholder="Enter name"
+            onChange={(e) => setName(e.target.value)}
+          ></Form.Control>
+        </FormGroup>
+        <FormGroup controlId="email">
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </FormGroup>
+        <FormGroup controlId="password">
+          <Form.Label>Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Enter password"
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </FormGroup>
+        <FormGroup controlId="confirmPassword">
+          <Form.Label>Confirm Password</Form.Label>
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            onChange={(e) => setConfirmPassword(e.target.value)}
+          ></Form.Control>
+        </FormGroup>
+        <Button type="submit" variant="primary">
+          Register
+        </Button>
+      </Form>
+      <Row className="py-3">
+        <Col>
+          Have an account?
+          <Link to={redirect ? `/login?redirect=${redirect}` : "/login"}>
+            {" "}
+            Login
+          </Link>
+        </Col>
+      </Row>
+    </FormContainer>
+  );
+};
+
+export default RegisterScreen;
